85836437245b3bf3e26314282322592cc45fcd74
Navesh Jaiswal <navesh.professional@gmail.com>
2025-08-02 20:37:48 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import("tailwindcss/v3").Config} */
+module.exports = {
+  darkMode: 'class', // or 'media'
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,html}'
+  ],
+  theme: {
+    extend: {
+      // custom theme tokens or overrides
+    }
+  },
+  plugins: [],
+};
